837f8b4f05506ecfac29648be2bcd59a3cabe968
DiddyChriss <diddychriss@gmail.com>
2021-03-22 10:58:48 +0100

    update static

--- BooksApp/settings.py
+++ BooksApp/settings.py
@@ -185,3 +185,7 @@ SIMPLE_JWT = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/BooksApp/static/',
+]
--- BooksApp/urls.py
+++ BooksApp/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', include('accounts.urls', namespace='accounts')),
     path('', include('api.urls', namespace='api')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
