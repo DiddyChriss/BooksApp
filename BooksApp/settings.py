5f381e079d1bb6ffa64c2803944a71563e33f8df
DiddyChriss <diddychriss@gmail.com>
2021-03-22 11:47:24 +0100

    update

--- BooksApp/settings.py
+++ BooksApp/settings.py
@@ -185,8 +185,5 @@ SIMPLE_JWT = {
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class BooksUrlModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    url = models.CharField(max_length=200, null=True)
+    url = models.CharField(max_length=1000, null=True)
     books_data = models.CharField(max_length=5000, null=True)
 
     def __str__(self):
@@ -12,14 +12,14 @@ class BooksUrlModel(models.Model):
 
 class BooksModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    book_id = models.CharField(max_length=100, null=True, unique=True)
-    title = models.CharField(max_length=100, null=True)
-    authors = models.CharField(max_length=100, null=True)
-    published_date = models.CharField(max_length=100, null=True)
-    categories = models.CharField(max_length=100, null=True)
-    average_rating = models.CharField(max_length=100, null=True)
-    ratings_count = models.CharField(max_length=100, null=True)
-    thumbnail = models.CharField(max_length=100, null=True)
+    book_id = models.CharField(max_length=300, null=True, unique=True)
+    title = models.CharField(max_length=300, null=True)
+    authors = models.CharField(max_length=300, null=True)
+    published_date = models.CharField(max_length=300, null=True)
+    categories = models.CharField(max_length=300, null=True)
+    average_rating = models.CharField(max_length=300, null=True)
+    ratings_count = models.CharField(max_length=300, null=True)
+    thumbnail = models.CharField(max_length=300, null=True)
 
     def __str__(self):
         return str(self.title)
